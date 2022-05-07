bad6c72254650231f95479a0b89fcd4c05e35fb0
ArthurNColA <82038526+ArthurNColA@users.noreply.github.com>
2022-05-07 13:08:51 +0200

    Add files via upload

---
+++ ClassBuzzer.ino
@@ -0,0 +1,79 @@
+#include "pitches.h"
+
+class Buzzer {
+
+  protected:
+  const int buzzerPin = D7;
+
+  public :
+  void beginBuzzer(){ 
+    pinMode(buzzerPin, OUTPUT);
+  }
+
+  void beep(int note, int duration){
+    tone(buzzerPin, note, duration);
+    delay(duration);
+  }
+
+  void song(){
+  beep(NOTE_A4, 500);
+  beep(NOTE_A4, 500);    
+  beep(NOTE_A4, 500);
+  beep(NOTE_F4, 350);
+  beep(NOTE_C5, 150);  
+  beep(NOTE_A4, 500);
+  beep(NOTE_F4, 350);
+  beep(NOTE_C5, 150);
+  beep(NOTE_A4, 650);
+ 
+  delay(500);
+ 
+  beep(NOTE_E5, 500);
+  beep(NOTE_E5, 500);
+  beep(NOTE_E5, 500);  
+  beep(NOTE_F5, 350);
+  beep(NOTE_C5, 150);
+  beep(NOTE_GS4, 500);
+  beep(NOTE_F4, 350);
+  beep(NOTE_C5, 150);
+  beep(NOTE_A4, 650);
+ 
+  delay(500);
+ 
+  beep(NOTE_A5, 500);
+  beep(NOTE_A4, 300);
+  beep(NOTE_A4, 150);
+  beep(NOTE_A5, 500);
+  beep(NOTE_GS5, 325);
+  beep(NOTE_G5, 175);
+  beep(NOTE_FS5, 125);
+  beep(NOTE_F5, 125);    
+  beep(NOTE_FS5, 250);
+ 
+  delay(325);
+ 
+  beep(NOTE_AS4, 250);
+  beep(NOTE_DS5, 500);
+  beep(NOTE_D5, 325);  
+  beep(NOTE_CS5, 175);  
+  beep(NOTE_C5, 125);  
+  beep(NOTE_AS4, 125);  
+  beep(NOTE_C5, 250);  
+ 
+  delay(350);
+  }
+
+  };
+
+Buzzer buzz;
+
+void setup() {
+  Serial.begin(74880);
+  Serial.print("coucou");
+  buzz.beginBuzzer();
+}
+
+void loop() {
+  buzz.song();
+
+}
